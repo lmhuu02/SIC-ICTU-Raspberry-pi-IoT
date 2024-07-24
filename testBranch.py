2a87d05512ac29e3d9959496abdba11dbc8446d4
LeHuu <lmhuu02@gmail.com>
2024-07-24 21:22:57 +0700

    commit to branch fix code num 1

--- testBranch.py
+++ testBranch.py
@@ -1,5 +1,5 @@
 '''
 xin chao day la lan commit 1 tai branch testBranch
-
 '''
 print("hello world!")
+print("sua code lan thu n va upload code len branch tam ") #sau day moi ghop voi branch chinh dung mage hoa rebase
